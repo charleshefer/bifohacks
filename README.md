6c197df9124c1904b92f7b884fbc5c4fac0d1c74
Charles Hefer <Charles.Hefer@agresearch.co.nz>
2025-01-20 14:58:31 +1300

    added custom merge

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # bifohacks
 
 Tips on bioinformatics activities
+
+Guides:
+    1) [customkegg.md]("./customkegg.md") How to perform KEGG annotation on a completely unannotated set of proteins
+
+
+Scripts:
+    1) [cmerge_ghostkoala_kegg_orthology.py](./scripts/cmerge_ghostkoala_kegg_orthology.py) Merge the GhostKoala and KEGG Orthology files to get a nice table of KEGG annotations for your proteins.
---
+++ scripts/cmerge_ghostkoala_kegg_orthology.py
@@ -0,0 +1,67 @@
+###############################################################################
+#Merging ghostkoala and kegg orthology files
+#
+#@author:charles.hefer@agresearch.co.nz
+#@version:0.1
+###############################################################################
+import optparse, sys
+import re
+
+ko_re = re.compile("(^K[0-9]+) +(.+)")
+
+def __main__():
+	"""Parse the cmd lne options"""
+	parser = optparse.OptionParser()
+	parser.add_option("-g", "--ghostkoala", default=None, dest="ghostkoala",
+					  help="The input ghostkoala file (user_ko.txt)")
+	parser.add_option("-k", "--kegg", default=None, dest="kegg_orthology",
+					  help="The input kegg orthology file")
+	parser.add_option("-o", "--output", default=None, dest="output",
+					  help="The output file")	
+	(options, args) = parser.parse_args()
+	
+	if not options.ghostkoala:
+		parser.error("Need to specify the input ghostkoala file")
+	if not options.kegg_orthology:
+		parser.error("Need to specify the input kegg orthology file")
+		
+
+	#Read the ghostkoala file
+	ghostkoala = {}
+	with open(options.ghostkoala, "r") as handle:
+		for line in handle:
+			try:
+				accession,ko = line.strip().split("\t")
+				if ko:
+					ghostkoala[accession] = ko
+			except ValueError:
+				pass
+	
+	#print(ghostkoala)
+
+	#read the kegg orthology file
+	kegg = {}
+	with open(options.kegg_orthology, "r") as handle:
+		#parse out the 4th column
+		for line in handle:
+			#print(line)
+			description = line.strip().split("\t")[3]
+			#print(description)
+			#sys.exit()
+			#the ko id is the first part before the space
+			ko_description = ko_re.match(description)
+			kegg[ko_description.groups()[0]] = ko_description.groups()[1]
+
+	#print(kegg)
+
+	#write the results
+	with open(options.output, "w") as handle:
+		for accession in ghostkoala.keys():
+			ko_term = ghostkoala[accession]
+			if ko_term in kegg.keys():
+				handle.write("%s\t%s\t%s\n" % (accession, ko_term, kegg[ko_term]))
+
+
+if __name__ == "__main__":
+	__main__()
+	
